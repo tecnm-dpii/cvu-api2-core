e42f3e356a17262872d5d43bda3e1b2104d14ff3
Francerz <francerzam@hotmail.com>
2022-11-24 14:44:40 -0600

    ADD model Estudiante

---
+++ src/Models/Estudiante.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace TecNM_DPII\CvuApi2Core\Models;
+
+class Estudiante
+{
+    /** @var int */
+    public $INST_ID;
+    /** @var int */
+    public $ID;
+    /** @var string */
+    public $CURP;
+    /** @var string */
+    public $MATRICULA_CONTROL;
+    /** @var string */
+    public $FECHA_NAC;
+    /** @var string */
+    public $APELLIDO_PATERNO;
+    /** @var string */
+    public $APELLIDO_MATERNO;
+    /** @var string */
+    public $NOMBRES;
+    /** @var string */
+    public $GENERO;
+    /** @var string */
+    public $EMAIL;
+    /** @var string */
+    public $NOMBRE_INSTITUCION;
+    /** @var string */
+    public $NOMBRE_CORTO_INSTITUCION;
+    /** @var string */
+    public $TIPO_INSTITUCION;
+    /** @var string */
+    public $ESTADO;
+    /** @var string */
+    public $NOMBRE_CARRERA;
+    /** @var string */
+    public $CLAVE_CARRERA;
+    /** @var int */
+    public $PROG_ID;
+    /** @var string */
+    public $MODALIDAD;
+    /** @var int */
+    public $SEMESTRE;
+    /** @var string */
+    public $NOMBRE_TIPO_NIVEL_EDUCATIVO;
+    /** @var float */
+    public $PROMEDIO_ACTUAL;
+    /** @var float */
+    public $PROMEDIO_GENERAL;
+    /** @var int */
+    public $CREDITOS_ACUMULADOS;
+    /** @var string */
+    public $INSCRIP_RESIDENCIAS;
+    /** @var string */
+    public $RFC;
+    /** @var string */
+    public $TELCELULAR;
+    /** @var string */
+    public $PROGRAMA_RECONOCIDO;
+    /** @var string */
+    public $SEGURO_ACCIDENTES;
+    /** @var string */
+    public $DISCAPACIDAD;
+    /** @var string */
+    public $TIPO_DISCAPACIDAD;
+    /** @var int|null */
+    public $ID_TIPO_DISCAPACIDAD;
+    /** @var null */
+    public $LENGUA_INDIGENA;
+    /** @var int */
+    public $EDAD;
+    /** @var string */
+    public $ESTADO_PROG;
+    /** @var string */
+    public $NIVEL;
+}
