fbdf8b6ee97d67cf0f62dbb788b5b425fbe6c2e3
Francerz <francerzam@hotmail.com>
2022-12-17 19:28:49 -0600

    ADD Adscripcion->plantel_tipo y constantes

--- src/Models/Cvu/Adscripcion.php
+++ src/Models/Cvu/Adscripcion.php
@@ -4,10 +4,16 @@ namespace TecNM_DPII\CvuApi2Core\Models\Cvu;
 
 class Adscripcion
 {
+    public const PLANTEL_TIPO_DIRECCION_GENERAL = 'Dirección General';
+    public const PLANTEL_TIPO_CENTRO_FEDERAL = 'Centro Federal';
+    public const PLANTEL_TIPO_INSTITUTO_FEDERAL = 'Instituto Federal';
+    public const PLANTEL_TIPO_DESCENTRALIZADO = 'Descentralizado';
+
     public $id_adscripcion;
     public $id_persona;
     public $id_plantel;
     public $plantel;
+    public $plantel_tipo;
     public $fecha_adscripcion;
     public $campus;
     public $edificio;
