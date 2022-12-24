517b781c81bbd6bc72e4b023d0bd5603de7dc0b3
Francerz <francerzam@hotmail.com>
2022-12-24 16:57:53 -0600

    ADD Models/Lgac3/Lgac

---
+++ src/Models/Lgacs3/Lgac.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace TecNM_DPII\CvuApi2Core\Models\Lgacs3;
+
+class Lgac
+{
+    public $id_lgac;
+    public $id_plantel;
+    public $id_nivel;
+    public $clave;
+    public $nombre;
+    public $nivel;
+    public $plantel;
+    public $plantel_abreviatura;
+}
