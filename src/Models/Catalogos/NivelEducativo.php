d568d49a7c54554cc910a5c8f06f198c3d4a55ca
Francerz <francerzam@hotmail.com>
2022-12-27 13:37:12 -0600

    ADD model Catalogos\NivelEducativo

---
+++ src/Models/Catalogos/NivelEducativo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace TecNM_DPII\CvuApi2Core\Models\Catalogos;
+
+class NivelEducativo
+{
+    public $id_nivel;
+    public $nombre;
+    public $es_posgrado;
+    public $masculino;
+    public $femenino;
+}
