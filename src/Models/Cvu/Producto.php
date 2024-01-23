748b57a9be5ad8230d521a60b9d35652751ac51d
Francerz <francerzam@hotmail.com>
2024-01-23 10:04:55 -0600

    ADD /models/cvu/Producto & ProductoTipo classes

---
+++ src/Models/Cvu/Producto.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TecNM_DPII\CvuApi2Core\Models\Cvu;
+
+class Producto
+{
+    public $id_producto_acad;
+    public $id_personas;
+    public $titulo;
+    public $autores;
+    public $descripcion;
+    public $fecha_publicacion;
+    public $tipo_producto;
+}
---
+++ src/Models/Cvu/ProductoTipo.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace TecNM_DPII\CvuApi2Core\Models\Cvu;
+
+abstract class ProductoTipo
+{
+    public const ARTICULO = 'Artículo';
+    public const LIBRO = 'Libro';
+    public const CAPITULO = 'Capítulo';
+    public const MEMORIA = 'Memoria';
+    public const PROTOTIPO = 'Prototipo';
+    public const TESIS_DIRIGIDA = 'Tesis dirigida';
+    public const PROPIEDAD_INDUSTRIAL = 'Propiedad industrial';
+    public const PROPIEDAD_AUTORAL = 'Propiedad autoral';
+    public const OTRO = 'Otro';
+}
