3cc05045a3f93f7ea3bf9cd0674e464337b17d1d
Francerz <10836837+francerz@users.noreply.github.com>
2026-01-15 15:50:37 -0600

    CHG matched /Lgacs2025/Autorizada to API backend

--- src/Models/Lgacs4/Autorizada.php
+++ src/Models/Lgacs4/Autorizada.php
@@ -2,12 +2,19 @@
 
 namespace TecNM_DPII\CvuApi2Core\Models\Lgacs4;
 
+use Francerz\JsonTools\JsonMap;
+use Francerz\JsonTools\JsonMappedInterface;
+use TecNM_DPII\CvuApi2Core\Models\Catalogos\Plantel;
+use TecNM_DPII\CvuApi2Core\Models\Catalogos\ProgramaEducativo;
+
 /**
- * @property-read string $plantel
- * @property-read string $programa
+ * @property-read string $id_area
+ * @property-read string $area
  * @property-read string $disciplina
+ * @property-read Plantel $Plantel
+ * @property-read ProgramaEducativo $Programa
  */
-class Autorizada
+class Autorizada implements JsonMappedInterface
 {
     public $id_linea;
     public $id_plantel;
@@ -15,4 +22,16 @@ class Autorizada
     public $id_disciplina;
     public $clave;
     public $nombre;
+
+    public static function getJsonMaps()
+    {
+        return [
+            new JsonMap('id_linea'),
+            new JsonMap('id_plantel'),
+            new JsonMap('id_programa'),
+            new JsonMap('id_disciplina'),
+            new JsonMap('clave'),
+            new JsonMap('nombre')
+        ];
+    }
 }
